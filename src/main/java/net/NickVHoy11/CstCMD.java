9a224fcf2d3974a3617b03533f1ee794b272a0c9
NickVHoy11 <76886743+NickVHoy11@users.noreply.github.com>
2021-12-05 15:12:11 -0500

    Basic Changes
    Setting Up Example Mod

---
+++ src/main/java/net/NickVHoy11/CstCMD.java
@@ -0,0 +1,11 @@
+package net.NickVHoy11;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CstCMD implements ModInitializer {
+
+    @Override
+    public void onInitialize(){
+        
+    }
+}
