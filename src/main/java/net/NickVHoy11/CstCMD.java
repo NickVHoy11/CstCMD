5bd45c6afa931fdf6aa65ab4a25c72bd250b81e6
NickVHoy11 <76886743+NickVHoy11@users.noreply.github.com>
2021-12-05 18:07:14 -0500

    1.18second commit

--- src/main/java/net/NickVHoy11/CstCMD.java
+++ src/main/java/net/NickVHoy11/CstCMD.java
@@ -2,10 +2,12 @@ package net.NickVHoy11;
 
 import net.fabricmc.api.ModInitializer;
 
+
 public class CstCMD implements ModInitializer {
 
     @Override
     public void onInitialize(){
-        
+        JSONObject CommandHolder = new JSONObject(); 
+
     }
 }
